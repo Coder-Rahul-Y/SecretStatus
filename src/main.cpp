1a6a86036794fffef9f31a8fa097ec786578df03
Coder-RY <hulracoc12345@gmail.com>
2022-04-25 18:58:55 +0530

    adding sound

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<SFML/Audio.hpp>
+#include<string.h>
+#include<Windows.h>
+
+using namespace std;
+
+int main(){
+
+    string msg;
+    // char str[501];
+    cout<<"Enter your secret message : ";
+    getline(cin, msg);
+
+    system("cls");
+    cin.get();
+    system("cls");
+
+
+    //audio
+    sf::SoundBuffer buf1, buf2, buf3;
+    buf1.loadFromFile("sound1.wav");
+    buf2.loadFromFile("sound2.ogg");
+    buf3.loadFromFile("sound3.wav");
+    sf::Sound snd[3];
+    snd[0].setBuffer(buf1);
+    snd[1].setBuffer(buf2);
+    snd[2].setBuffer(buf3);
+
+
+    for (int i = 0; i < msg.length(); i++)
+    {
+        cout<<"\b "<<msg[i];
+
+        int j = rand()%3;
+        snd[j].play();
+        if (msg[i]==' ') Sleep(30);
+
+        Sleep(100);
+    }
+
+    system("cls");
+    // cout<<"Major GR\n";
+    cin.get();
+
+
+    return 0;
+}
